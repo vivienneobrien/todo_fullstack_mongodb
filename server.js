a786a80285d6da8ba3110390127a11de12e93f7f
vivienneobrien <vivienneobrienis@gmail.com>
2024-01-25 11:40:33 +0000

    fix: small update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send("hello node api")
+    res.send("hello node api- small test")
 })
 
 app.listen(3000, () => {
